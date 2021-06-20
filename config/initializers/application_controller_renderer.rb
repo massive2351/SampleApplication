1f5fa8ab4c247e794828debeb76485671a7911da
massive2351 <massive2351@gmail.com>
2021-06-20 12:14:14 +0000

    画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '12e6a8ff0b3ab5e5f61c70047cee26d07df8e39542274c99686db7880f6bd40bc6e05ecf7b404d56e6f5159292b26fcf039e513ede5dd1a4c962ba48d8ef2695'
